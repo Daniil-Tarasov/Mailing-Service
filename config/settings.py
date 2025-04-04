734efd1d388578723f9277e2383a7683e0d70d89
Daniil Tarasov <danya.tarasov.01@mail.ru>
2025-04-05 00:03:15 +0300

    Sml fxs

--- config/settings.py
+++ config/settings.py
@@ -152,6 +152,13 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
+CACHES = {
+    'default': {
+        'BACKEND': os.getenv("CACHES_BACKEND"),
+        'LOCATION': os.getenv("CACHES_LOCATION"),
+    }
+}
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
